25839b6f711d4d9ae4fa77ea408b90f7abd3031f
Evelyn Berenice Torres Perez <torres2907_th@outlook.com>
2020-05-18 01:46:49 -0500

    Se actualizo home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  infor = [
+    {
+      nombre:'Ariana Grande',
+      img: 'assets/img/ariana.jpg ',
+      des:'Premios',
+      fuente: 'Cantante y actriz Ariana Grande ha ganado más de 102 premios durante su carrera. En 2019, Grande lanzó su quinto álbum de estudio, Thank U, Siguiente , que incluye el primer sencillo Thank u next . Grande fue nominada para dos premios en las 61ª Entrega Anual del Grammy , ganando su primer premio al Mejor Álbum Vocal Pop por Edulcorante .'
+    },
+    {
+      nombre:'Jonas Brothers',
+      img: ' assets/img/jonas.jpg ',
+      des:'Grammys',
+      fuente:'Los integrantes de la agrupación de música pop estadounidense Jonas Brothers, se dijeron tristes por tener que '
+    },
+    {
+      nombre:'Dua lipa',
+      img: 'assets/img/dualipa.jpg ',
+      des:'Dua Lipa ha vuelto a cambiar de color de pelo y ahora lo lleva en un tono muy acorde con esta temporada',
+      fuente:'Desde que empezó la cuarentenaDua Lipa no ha parado de experimentar con su cabello. '
+    }
+  ];
+
+  vertical={
+    direction:"vertical",
+  };
+ 
+  horizontal={
+    direction:"horizontal",
+    loop: true,
+  };  
+
   constructor() {}
 
 }
